1012f529472167cf7543d9e86ba230eb30ad0960
i-am-HRISHABH <csecec.1803158@gmail.com>
2022-09-10 12:49:20 +0530

    9th commit sub catagory extended 100%

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -4,21 +4,39 @@ import "./Category.css";
 const Category = (props) => {
   const setColor = (val) => {
     if (val === "Fire") {
-      return "red";
+      return "#FF6D03";
     } else if (val === "Poison") {
-      return "purple";
+      return "#ad00f";
     } else if (val === "Water") {
-      return "blue";
+      return "#3E8AFC";
     } else if (val === "Bug") {
-      return "crimson";
+      return "#770F41";
     } else if (val === "Flying") {
-      return "pink";
+      return "#0515A1";
     } else if (val === "Electric") {
-      return "aqua";
+      return "#FFF500";
     } else if (val === "Ground") {
-      return "brown";
+      return "#934702";
     } else if (val === "Grass") {
-      return "green";
+      return "#934702";
+    } else if (val === "Ice") {
+      return "#7EECFB";
+    } else if (val === "Psychic") {
+      return "#08D67F";
+    } else if (val === "Dark") {
+      return "#210230";
+    } else if (val === "Steel") {
+      return "#79747B";
+    } else if (val === "Ghost") {
+      return "#F56D6D";
+    } else if (val === "Rock") {
+      return "#A59C95";
+    } else if (val === "Fighting") {
+      return "#1D7C77";
+    } else if (val === "Fairy") {
+      return "#FD057C";
+    } else if (val === "Dragon") {
+      return "#FF0000";
     } else {
       return "black";
     }
